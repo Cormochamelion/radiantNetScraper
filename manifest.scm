Cormochamelion/radiantNetScraping

manifest.scm: Add hatchling
  For managing package metadata and build info.

fronius_scraper/manifest.scm
@@ -7,6 +7,7 @@
 (specifications->manifest
   (list "python-beautifulsoup4"
         "python-dotenv"
+        "python-hatch"
         "python-pip"
         "python-requests"
         "python"))

Cormochamelion/radiantNetScraper

manifest.scm
@@ -7,6 +7,7 @@
 (specifications->manifest
   (list "python-beautifulsoup4"
         "python-dotenv"
+        "python-hatchling"
         "python-pip"
         "python-requests"
         "python"))