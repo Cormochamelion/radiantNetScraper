a914fb0d7d4f97b60357940cca3a5ef9533f1285
cormochamelion <cormocodran1.5@gmail.com>
2024-10-27 16:33:34 +0100

    Add channels.scm

---
+++ channels.scm
@@ -0,0 +1,15 @@
+(list (channel
+        (name 'guix)
+        (url "https://git.savannah.gnu.org/git/guix.git")
+        (branch "master")
+        (commit
+          "b522b468cbefcae6170e260ffadadb57f6f5ca54")
+        (introduction
+          (make-channel-introduction
+            "9edb3f66fd807b096b48283debdcddccfea34bad"
+            (openpgp-fingerprint
+              "BBB0 2DDF 2CEA F6A8 0D1D  E643 A2A0 6DF2 A33A 54FA"))))
+      (channel
+        (name 'guix-cran)
+        (url "https://github.com/guix-science/guix-cran.git")
+        (commit "007648bb33aae2db137bf6b38e5d220361cc960a")))
