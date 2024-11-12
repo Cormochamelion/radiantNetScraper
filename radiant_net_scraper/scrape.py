cbfc4a08caf8542892a8bc55f8298823db9e1375
cormochamelion <cormocodran1.5@gmail.com>
2024-11-12 14:48:11 +0100

    scraper: Use config

--- radiant_net_scraper/scrape.py
+++ radiant_net_scraper/scrape.py
@@ -1,8 +1,8 @@
 import os
 import json
 import datetime as dt
-from dotenv import load_dotenv
 
+from radiant_net_scraper.config import Config, get_config_paths
 from radiant_net_scraper.fronius_session import FroniusSession
 
 
@@ -36,13 +36,39 @@ def get_secrets_from_env() -> dict:
     return secrets
 
 
+def get_fronius_secrets() -> dict:
+    """
+    Obtain a dict of required secrets for fronius, in this case from the config object.
+    """
+    config = Config.get_config()
+
+    secrets = {
+        "username": config["secrets"]["username"],
+        "password": config["secrets"]["password"],
+        "fronius-id": config["secrets"]["fronius-id"],
+    }
+
+    if None in secrets.values():
+        none_secrets = ", ".join([x[0] for x in secrets.items() if x[1] is None])
+
+        config_paths = get_config_paths()
+        user_config = config_paths["user"]
+        site_config = config_paths["site"]
+
+        raise ValueError(
+            f"Fields {none_secrets} were not filled. Ensure you set the secrets "
+            f"either in the environment, the machine-wide config at {site_config}, or "
+            f"your personal config at {user_config}."
+        )
+
+    return secrets
+
+
 def run_scraper(output_dir: str = "./", days_ago: int = 1):
     """
     Load required secrets and save the daily usage data to an output dir.
     """
-    load_dotenv()
-
-    secrets = get_secrets_from_env()
+    secrets = get_fronius_secrets()
 
     date_to_parse = dt.date.today() - dt.timedelta(days=days_ago)
 
