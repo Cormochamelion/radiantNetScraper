Cormochamelion/radiantNetScraping

scraper: Add script for data parsing

fronius_scraper/fronius_scraper/scripts.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 import argparse
 from fronius_scraper.scrape import run_scraper
+from fronius_scraper import data_parser
 
 
 def scrape():
@@ -37,3 +38,32 @@ def scrape():
     args = argparser.parse_args()
 
     run_scraper(**vars(args))
+
+
+def parse_json_files():
+    argparser = argparse.ArgumentParser(
+        "fronius-parse-json-files",
+        description=("Parse scraped JSON files into a usable format."),
+    )
+
+    argparser.add_argument(
+        "--input-dir",
+        "-i",
+        default="./",
+        type=str,
+        # TODO Add info on filename pattern.
+        help="Dir in which to search for JSON files to parse (default: %(default)s).",
+    )
+
+    argparser.add_argument(
+        "--output-dir",
+        "-o",
+        default="./",
+        type=str,
+        # TODO Add info on what will be outputted.
+        help="Dir to which output will be saved (default: %(default)s).",
+    )
+
+    args = argparser.parse_args()
+
+    data_parser.main(output_dir=args.output_dir, input_dir=args.input_dir)

Cormochamelion/radiantNetScraper

radiant_net_scraper/scripts.py
@@ -3,6 +3,7 @@ import argparse
 
 from radiant_net_scraper.config import get_chosen_raw_data_path
 from radiant_net_scraper.scrape import run_scraper
+from radiant_net_scraper import data_parser
 
 
 def scrape():
@@ -39,3 +40,32 @@ def scrape():
     args = argparser.parse_args()
 
     run_scraper(**vars(args))
+
+
+def parse_json_files():
+    argparser = argparse.ArgumentParser(
+        "fronius-parse-json-files",
+        description=("Parse scraped JSON files into a usable format."),
+    )
+
+    argparser.add_argument(
+        "--input-dir",
+        "-i",
+        default="./",
+        type=str,
+        # TODO Add info on filename pattern.
+        help="Dir in which to search for JSON files to parse (default: %(default)s).",
+    )
+
+    argparser.add_argument(
+        "--output-dir",
+        "-o",
+        default="./",
+        type=str,
+        # TODO Add info on what will be outputted.
+        help="Dir to which output will be saved (default: %(default)s).",
+    )
+
+    args = argparser.parse_args()
+
+    data_parser.main(output_dir=args.output_dir, input_dir=args.input_dir)