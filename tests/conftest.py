4af99f11e074a0cb311947e4be4131cfa03189c6
cormochamelion <cormocodran1.5@gmail.com>
2024-12-01 17:22:29 +0100

    Factor get_fronius_secrets to config
      Now needed by more modules.

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,7 @@
 from json import load
 from pytest_cases import fixture
 
+import radiant_net_scraper.scrape as scrape
 from radiant_net_scraper.fronius_session import FroniusSession
 from test_infra.common_test_infra import arbitrary_json_test_file
 
@@ -11,10 +12,9 @@ def arbitrary_file_dummy_fronius_session(monkeypatch) -> None:
     Monkeypatch FroniusSession to not do any requests, and just return the
     contents of an arbitrary test JSON file when asked for a chart.
     """
-    env_dict = {"username": "dummy", "password": "dummy", "fronius_id": "dummy"}
+    dummy_secrets = {"username": "dummy", "password": "dummy", "fronius-id": "dummy"}
 
-    for env_name, env_value in env_dict.items():
-        monkeypatch.setenv(env_name, env_value)
+    monkeypatch.setattr(scrape, "get_fronius_secrets", lambda: dummy_secrets)
 
     with open(arbitrary_json_test_file(), encoding="UTF-8") as infile:
         source_json_data = load(infile)
