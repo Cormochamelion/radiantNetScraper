12735bf9fdc0d69d1e5f54ecd75dfbaaef47e903
cormochamelion <cormocodran1.5@gmail.com>
2024-12-03 16:43:27 +0100

    Use singleton for fronius session
      To avoid needlessly repeating the login step.

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,8 +1,8 @@
 from json import load
 from pytest_cases import fixture
 
-import radiant_net_scraper.scrape as scrape
-from radiant_net_scraper.fronius_session import FroniusSession
+import radiant_net_scraper.fronius_session as fsession
+from radiant_net_scraper.fronius_session import _FroniusSession
 from test_infra.common_test_infra import arbitrary_json_test_file
 
 
@@ -14,14 +14,14 @@ def arbitrary_file_dummy_fronius_session(monkeypatch) -> None:
     """
     dummy_secrets = {"username": "dummy", "password": "dummy", "fronius-id": "dummy"}
 
-    monkeypatch.setattr(scrape, "get_fronius_secrets", lambda: dummy_secrets)
+    monkeypatch.setattr(fsession, "get_fronius_secrets", lambda: dummy_secrets)
 
     with open(arbitrary_json_test_file(), encoding="UTF-8") as infile:
         source_json_data = load(infile)
 
-    monkeypatch.setattr(FroniusSession, "login", lambda *args, **kwargs: None)
+    monkeypatch.setattr(_FroniusSession, "login", lambda *args, **kwargs: None)
     monkeypatch.setattr(
-        FroniusSession, "get_chart", lambda *args, **kwargs: source_json_data
+        _FroniusSession, "get_chart", lambda *args, **kwargs: source_json_data
     )
 
     return None
