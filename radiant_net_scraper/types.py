0b3a68f3877b0fe6f43e9fb33b93ecc055a0a601
cormochamelion <cormocodran1.5@gmail.com>
2024-12-03 23:14:31 +0100

    Move types to extra module

---
+++ radiant_net_scraper/types.py
@@ -0,0 +1,11 @@
+import pandas as pd
+from typing import NamedTuple
+
+
+class OutputDataFrames(NamedTuple):
+    """
+    Named tuple to keep a days data grouped together.
+    """
+
+    raw: pd.DataFrame
+    aggregated: pd.DataFrame
