b86541a9773433fb9aaa620ef75d7f7ee733c45a
cormochamelion <cormocodran1.5@gmail.com>
2024-12-22 17:23:30 +0100

    ingestion_flow: Use ChartFileGroups

--- radiant_net_scraper/ingestion_flow.py
+++ radiant_net_scraper/ingestion_flow.py
@@ -2,11 +2,12 @@
 Run the full flow of a day's scraping and ingestion into the database.
 """
 
+from dataclasses import astuple
 from datetime import datetime, timedelta
 
 from apscheduler.schedulers.blocking import BlockingScheduler
 
-from radiant_net_scraper.data_parser import parse_json_data_from_file_list
+from radiant_net_scraper.data_parser import parse_json_data_from_file_pair_list
 from radiant_net_scraper.scrape import run_scraper
 
 
@@ -21,7 +22,7 @@ def ingest_day(
     parsing_kwargs = parsing_kwargs or {}
 
     output_files = run_scraper(**scraping_kwargs)
-    parse_json_data_from_file_list(output_files.values(), **parsing_kwargs)
+    parse_json_data_from_file_pair_list(list(astuple(output_files)), **parsing_kwargs)
 
 
 def run_ingestion_continuously() -> None:
